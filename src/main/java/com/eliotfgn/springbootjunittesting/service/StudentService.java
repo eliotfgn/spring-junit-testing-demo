2d6cc11958421930153460a94a8b1f8aba6a65f9
Eliot Fagnon <efagnon2002@gmail.com>
2022-10-09 21:15:19 +0100

    feat: add create, read and delete services for students

--- src/main/java/com/eliotfgn/springbootjunittesting/service/StudentService.java
+++ src/main/java/com/eliotfgn/springbootjunittesting/service/StudentService.java
@@ -1,11 +1,35 @@
 package com.eliotfgn.springbootjunittesting.service;
 
+import com.eliotfgn.springbootjunittesting.model.Student;
 import com.eliotfgn.springbootjunittesting.repository.StudentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class StudentService {
     private final StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addStudent(Student student) {
+        Boolean takenEmail = studentRepository.selectExistEmail(student.getEmail());
+
+        if (takenEmail) {
+            throw new RuntimeException("Email is already taken!");
+        }
+
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long id) {
+        if (!studentRepository.existsById(id)) {
+            throw new RuntimeException("No such user exists.");
+        }
+        studentRepository.deleteById(id);
+    }
 }
---
+++ src/test/java/com/eliotfgn/springbootjunittesting/service/StudentServiceTest.java
@@ -0,0 +1,30 @@
+package com.eliotfgn.springbootjunittesting.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllStudents() {
+    }
+
+    @Test
+    void addStudent() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+}
