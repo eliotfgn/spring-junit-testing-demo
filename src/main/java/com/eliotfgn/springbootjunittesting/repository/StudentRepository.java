8c010c32a86cfd125c9532f61324f9b1f94aa8fe
Eliot Fagnon <efagnon2002@gmail.com>
2022-10-09 20:49:50 +0100

    feat: verify if an email exists in the database

--- src/main/java/com/eliotfgn/springbootjunittesting/repository/StudentRepository.java
+++ src/main/java/com/eliotfgn/springbootjunittesting/repository/StudentRepository.java
@@ -2,6 +2,15 @@ package com.eliotfgn.springbootjunittesting.repository;
 
 import com.eliotfgn.springbootjunittesting.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Query("" +
+            "select case " +
+            "when count(s) > 0 then true " +
+            "else false end " +
+            "from Student s " +
+            "where s.email=?1")
+    Boolean selectExistEmail(String email);
 }
